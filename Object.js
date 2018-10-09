12df36d6e769633de1be095fb53f804de42780df
Nicolle Loayza <nicolleloayza@Nicolles-MacBook-Pro.local>
2018-10-08 22:52:43 -0400

    Objects Assignment

---
+++ Object.js
@@ -0,0 +1,57 @@
+// 1.
+// let cat = {
+//   genus: 'Felis',
+//   species: 'Catus'
+// }
+//
+// console.log(cat.species)
+
+// 2.
+// let cat = {
+//   genus: 'Felis',
+//   species: 'Catus',
+//   color: 'red'
+// }
+
+// 3.
+// let cat = {
+//   genus: 'Felis',
+//   species: 'Catus'
+// }
+// if(cat.texture === undefined){
+// console.log("No texture")
+// } else {
+//   console.log("The property is there")
+// }
+// 4.
+// let films = [
+//  {
+//    name: 'Psycho',
+//    director: 'Alfred Hitchcock',
+//    released: 1960
+//  }, {
+//    name: 'Citizen Kane',
+//    director: 'Orson Welles',
+//    released: 1941
+//  }, {
+//    name: 'The Usual Suspects',
+//    director: 'Bryan Singer',
+//    released: 1995
+//  }
+// ]
+// for(let i = 0; i < films.length; i++){
+//   console.log(films[i].director)
+// }
+
+// 5.
+// False
+
+6.
+
+let recipe =
+  {
+  name: 'Grilled Cheese',
+  serving: 1,
+  ingredients: ['Cheese', 'Bread', 'Butter']
+}
+console.log(recipe)
