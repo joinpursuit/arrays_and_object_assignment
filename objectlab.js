7756169c72dae7cf9a3548d27669a4a815147573
Jenesh <jenesh.napit@gmail.com>
2019-07-14 09:33:12 -0400

    pull me

--- objectlab.js
+++ objectlab.js
@@ -149,8 +149,8 @@ for (let i = 0; i < firstName.length; i++) {
     fullName.push(lastName[i]);
 }
 
-console.log("6) ", firstName);
-console.log("6) ", fullName);
+console.log("6a) ", firstName);
+console.log("6b) ", fullName);
 
 console.log("- - - - - - -  - - -  - - - - - -  - -  -   - -  -  -- - -    -   -");
 // ## Question 7
@@ -221,12 +221,31 @@ for (let key in deposits) {
 }
 
 let stolenCents = [];
+for (let key in deposits) {
+    for(num of deposits[key]) {
+        let hold = num - Math.floor(num);
+        stolenCents.push(hold);
+    }
+}
 
+for (let key in deposits) {
+    let roundDown = [];
+    for (num of deposits[key]) {
+        num = Math.floor(num);
+        roundDown.push(num);
+    }
+    deposits[key] = roundDown;
+    console.log(roundDown);
+}
 
+let stolenTotal = 0;
+for (let num of stolenCents) {
+    stolenTotal += num;
+}
 
 console.log("8a) ", maxPerson);
-console.log("8b) ", );
-console.log("8c) ", );
+console.log("8b) ", "Stolen: ", stolenCents , "\n\nOriginal Object Rounded Down: ", deposits);
+console.log("8c) ", stolenTotal);
 
 console.log("- - - - - - -  - - -  - - - - - -  - -  -   - -  -  -- - -    -   -");
 // ## Question 9
@@ -238,9 +257,17 @@ console.log("- - - - - - -  - - -  - - - - - -  - -  -   - -  -  -- - -    -   -
 // name: Mole
 // servings: 2
 // ingredients: cinnamon, cumin, cocoa
-// ```
+// ```  
 
+let obj = {
+    "name" : "Tacos",
+    "servings" : 2,
+    "ingredients" : ["cinnamon", "cumin", "cocoa"]
+}
 
+for (let key in obj) {
+    console.log("9) ", key, ": ",  obj[key]);
+}
 
 console.log("- - - - - - -  - - -  - - - - - -  - -  -   - -  -  -- - -    -   -");
 // ## Question 10
@@ -250,9 +277,25 @@ console.log("- - - - - - -  - - -  - - - - - -  - -  -   - -  -  -- - -    -   -
 // `You already watched "Wonder Woman" directed by Patty Jenkins`
 // `You still need to watch "Wonder Woman" by director Patty Jenkins. `
 
+let movieStatus =[ 
+    {
+        title: "The Dark Knight Rises",
+        director: "Christopher Nolan",
+        watchStatus: true,
+    }, {
+        title: "Batman Begins",
+        director: "Christopher Nolan",
+        watchStatus: false,
+    }
+];
 
-
-
+for (let movie of movieStatus) {
+    if (movie["watchStatus"]) {
+        console.log("10) ", `You already watched ${movie["title"]} directed by ${movie["director"]}.`);
+    } else {
+        console.log("10) ", `You still need to watch ${movie["title"]} directed by ${movie["director"]}.`);
+    }
+}
 
 console.log("- - - - - - -  - - -  - - - - - -  - -  -   - -  -  -- - -    -   -");
 // ## Question 11
@@ -260,30 +303,57 @@ console.log("- - - - - - -  - - -  - - - - - -  - -  -   - -  -  -- - -    -   -
 // Given the following exert from the Declaration of Independence, find the most frequent word that is longer than 5 characters.
 
 // ```js
-// const declarationOfIndependence = `
-// When in the Course of human events, it becomes necessary for one people to dissolve the
-// political bands which have connected them with another, and to assume among the powers of the
-// earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle
-// them, a decent respect to the opinions of mankind requires that they should declare the causes
-// which impel them to the separation.
+const declarationOfIndependence = `
+When in the Course of human events, it becomes necessary for one people to dissolve the
+political bands which have connected them with another, and to assume among the powers of the
+earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle
+them, a decent respect to the opinions of mankind requires that they should declare the causes
+which impel them to the separation.
 
-// We hold these truths to be self-evident, that all men are created equal, that they are endowed by
-// their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit
-// of Happiness. That to secure these rights, Governments are instituted among Men, deriving
-// their just powers from the consent of the governed, That whenever any Form of Government
-// becomes destructive of these ends, it is the Right of the People to alter or to abolish it, and to
-// institute new Government, laying its foundation on such principles and organizing its powers in
-// such form, as to them shall seem most likely to effect their Safety and Happiness. Prudence,
-// indeed, will dictate that Governments long established should not be changed for light and
-// transient causes; and accordingly all experience hath shewn, that mankind are more disposed to
-// suffer, while evils are sufferable, than to right themselves by abolishing the forms to which they
-// are accustomed. But when a long train of abuses and usurpations, pursuing invariably the same
-// Object evinces a design to reduce them under absolute Despotism, it is their right, it is their duty,
-// to throw off such Government, and to provide new Guards for their future security. Such has
-// been the patient sufferance of these Colonies; and such is now the necessity which constrains
-// them to alter their former Systems of Government. The history of the present King of Great
-// Britain is a history of repeated injuries and usurpations, all having in direct object the
-// establishment of an absolute Tyranny over these States. To prove this, let Facts be submitted to a
-// candid world.
+We hold these truths to be self-evident, that all men are created equal, that they are endowed by
+their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit
+of Happiness. That to secure these rights, Governments are instituted among Men, deriving
+their just powers from the consent of the governed, That whenever any Form of Government
+becomes destructive of these ends, it is the Right of the People to alter or to abolish it, and to
+institute new Government, laying its foundation on such principles and organizing its powers in
+such form, as to them shall seem most likely to effect their Safety and Happiness. Prudence,
+indeed, will dictate that Governments long established should not be changed for light and
+transient causes; and accordingly all experience hath shewn, that mankind are more disposed to
+suffer, while evils are sufferable, than to right themselves by abolishing the forms to which they
+are accustomed. But when a long train of abuses and usurpations, pursuing invariably the same
+Object evinces a design to reduce them under absolute Despotism, it is their right, it is their duty,
+to throw off such Government, and to provide new Guards for their future security. Such has
+been the patient sufferance of these Colonies; and such is now the necessity which constrains
+them to alter their former Systems of Government. The history of the present King of Great
+Britain is a history of repeated injuries and usurpations, all having in direct object the
+establishment of an absolute Tyranny over these States. To prove this, let Facts be submitted to a
+candid world.
 // `
 // ```
+
+let decObj = {};
+let wordArr = declarationOfIndependence.replace(/[\n\t\r\,\;\.\-]/g," ").replace(/[\']/g,"").toLowerCase().split(" ");
+
+for (let word of wordArr) {
+    if (decObj.hasOwnProperty(word)) {
+        decObj[word] += 1;
+    } else {
+        decObj[word] = 1;
+    }
+}
+
+let maxWord = 0;
+let maxWordLtr = '';
+for (let word in decObj) {
+    if (word === '') {
+        decObj[word] = 0;
+    }
+    if (decObj[word] > maxWord && decObj[word] > 5) {
+        maxWord = decObj[word];
+        maxWordLtr = word;
+    } else if (decObj[word] === maxWord && decObj[word] > 5){
+        maxWordLtr += " & " + word;
+    }
+}
+
+console.log("11) ", maxWordLtr);
