bed00f60d6f95ec7497b8d89dae0ebfda00d45c5
sergiocohens <sergiocohensalama@pursuit.org>
2019-07-14 16:09:11 -0400

    lab

--- objects_lab.js
+++ objects_lab.js
@@ -150,37 +150,50 @@ console.log(fullNames)
 //Print the second most common letter in the string below:
 
 var myString = "We're flooding people with information. We need to feed it through a processor. A human must turn information into intelligence or knowledge. We've tended to forget that no computer will ever ask a new question."
-let container = []
-let stringArray = myString.split("")
-for (let i = 0; i < stringArray.length ; i++) {
-  if (stringArray[i] === stringArray[i + 1]) {
-  container.push(stringArray[i])
+myString = myString.toLocaleLowerCase()
+let container = {}
+for (let g = 0; g < myString.length; g++) {
+  let char = myString[g]
+  if (char !== " " && char !== "." && char!=="'")
+  if (container[char] === undefined) {
+    container[char] = 1
+  } else {
+    container[char]++
   }
 }
-console.log(container)
+let common
+let count = 0
+for (let h in container) {
+  if (container[h] > count) {
+    count = container[h]
+    common = h
+  }
+}
+let second
+count = 0
+for (let i in container) {
+  if (container[i] > count & container[i] < container[common]) {
+    count = container[i]
+    second = i
+  }
+}
+console.log(second)
 
 // ## Question 8
 //
 // You are given the object `deposits`, which maps a persons name to an array of deposits that have been made to their account.
 //
 // ```js
-let deposits = {
- "Williams" : [300.65, 270.45, 24.70, 52.00, 99.99],
- "Cooper" : [200.56, 55.00, 600.78, 305.15, 410.76, 35.00],
- "Davies" : [400.98, 56.98, 300.00],
- "Clark" : [555.23, 45.67, 99.95, 80.76, 56.99, 46.50, 265.70],
- "Johnson" : [12.56, 300.00, 640.50, 255.60, 26.88]
-}
+// let deposits = {
+//  "Williams" : [300.65, 270.45, 24.70, 52.00, 99.99],
+//  "Cooper" : [200.56, 55.00, 600.78, 305.15, 410.76, 35.00],
+//  "Davies" : [400.98, 56.98, 300.00],
+//  "Clark" : [555.23, 45.67, 99.95, 80.76, 56.99, 46.50, 265.70],
+//  "Johnson" : [12.56, 300.00, 640.50, 255.60, 26.88]
+// }
 // ```
 // a) Write code to to print the name and total amount deposited of the person who received the most money.
-let greater = 0
-for (g in deposits) {
-  for (h of g) {
-  if (h > greater)
-  greater = greater + h
-}
-}
-console.log(greater)
+//
 // b) Create an array called `stolenCents`, iterate over deposits for each person and steal their cents! ... like Office Space or Superman 3. Calculate the decimal part of each value, add it to the `stolenCents` array and round down the value in the original object.
 //
 // c) How much money did you steal?
